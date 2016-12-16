66d4db33b1a050133ad215a11f7b914906842e83
Patryk Grela <Patryk.Grela@sollers.eu>
2016-12-16 07:59:31 +0100

    tabs resized

--- js/scripts.js
+++ js/scripts.js
@@ -147,15 +147,53 @@ bestUI.controller('BestUIController', ['$scope', function ($scope) {
         {text: "Quotation"}
       ]
     });
-  }
+  };
+
+    var waiting;
+  $scope.activateHints = function(){
+
+      editor.on("change", function () {
+          clearTimeout(waiting);
+          waiting = setTimeout(updateHints, 500);
+      });
+  };
+
+  $scope.updateHints = function () {
+          editor.operation(function () {
+              for (var i = 0; i < widgets.length; ++i) {
+                  editor.removeLineWidget(widgets[i]);
+              }
+              widgets.length = 0;
+
+              JSHINT(editor.getValue());
+              for (var i = 0; i < JSHINT.errors.length; ++i) {
+                  var err = JSHINT.errors[i];
+                  if (!err) {
+                      continue;
+                  }
+                  var msg = document.createElement("div");
+                  var icon = msg.appendChild(document.createElement("span"));
+                  icon.innerHTML = "!!";
+                  icon.className = "lint-error-icon";
+                  msg.appendChild(document.createTextNode(err.reason));
+                  msg.className = "lint-error";
+                  widgets.push(editor.addLineWidget(err.line - 1, msg, {coverGutter: false, noHScroll: true}));
+              }
+          });
+          var info = editor.getScrollInfo();
+          var after = editor.charCoords({line: editor.getCursor().line + 1, ch: 0}, "local").top;
+          if (info.top + info.clientHeight < after) {
+              editor.scrollTo(null, after - info.clientHeight + 3);
+          }
+      }
 
 }]);
 
 $("#horizontal").kendoSplitter({
   panes: [
-    {collapsible: true},
+    {collapsible: true, size:'20%'},
     {collapsible: false},
-    {collapsible: true}
+    {collapsible: true, size:'20%'}
   ]
 });
 
